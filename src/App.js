37543347654cfb37e60c56d7bad418a10435eb23
gabriele-andolina <gabe.andolina@gmail.com>
2023-09-21 19:17:22 +0200

    Components: Created BillInput

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import "./style.css";
 
 export default function App() {
-    return <GrandTotal></GrandTotal>;
+    return (
+        <div>
+            <BillInput />
+            <GrandTotal />
+        </div>
+    );
 }
 
 function GrandTotal() {
     return <h1>You pay €100 (€90 + €10 tip)</h1>;
+}
+
+function BillInput() {
+    return (
+        <div>
+            <h3>How much was the bill?</h3>
+            <input type="number"></input>
+        </div>
+    );
 }
