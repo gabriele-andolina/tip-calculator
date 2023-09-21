a89e3f07d0b09080abdb0686a5a36a611747a8ec
gabriele-andolina <gabe.andolina@gmail.com>
2023-09-21 19:22:47 +0200

    Components: Created ServiceInput

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
     return (
         <div>
             <BillInput />
+            <ServiceInput />
             <GrandTotal />
         </div>
     );
@@ -18,6 +19,20 @@ function BillInput() {
         <div>
             <h3>How much was the bill?</h3>
             <input type="number"></input>
+        </div>
+    );
+}
+
+function ServiceInput() {
+    return (
+        <div>
+            <h3>How was the service?</h3>
+            <select>
+                <option>Awful (0%)</option>
+                <option>Okay (5%)</option>
+                <option>Good (10%)</option>
+                <option>Superb (20%)</option>
+            </select>
         </div>
     );
 }
